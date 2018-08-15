43f7f36ce31e481a4354c8330290a649e4ae8f5c
Linh <ltmylinh2606@gmail.com>
2018-08-15 11:52:08 -0400

    Extracting components

--- react-calculator/src/app/app.test.js
+++ react-calculator/src/app/app.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+		const div = document.createElement('div');
+		ReactDOM.render(
+				<App/>, div);
+		ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders a snapshot', () => {
+		const tree = renderer
+				.create(<App/>)
+				.toJSON();
+		expect(tree).toMatchSnapshot();
+})
