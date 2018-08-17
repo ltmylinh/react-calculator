5eb637fb192adc807ae3f60234f8c317060a5953
Linh <ltmylinh2606@gmail.com>
2018-08-17 00:08:56 -0400

    add enzyme testing

--- react-calculator/src/components/calculator/calculator.test.js
+++ react-calculator/src/components/calculator/calculator.test.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import Calculator from './calculator';
 
-it('render without crashing', () => {});
+it('render without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(
+				<Calculator/>, div);
+		ReactDOM.unmountComponentAtNode(div);
+});
 
 it('create a calculator snapshot', () => {});
